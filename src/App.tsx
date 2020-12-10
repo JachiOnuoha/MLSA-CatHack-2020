6afd36d1bf1b3914950467e74d9bdad4a9535c65
Jachimike Onuoha <jon226@g.uky.edu>
2020-12-10 16:18:09 -0500

    Skeleton of API request and UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { PrimaryButton} from 'office-ui-fabric-react';
+import { TextField } from 'office-ui-fabric-react/lib/TextField';
 
 // Just a comment
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App: React.FC = () => {
+  const apiKey = '6278138fbcab4a0592dcc44774ec259f';
+  const endpoint = 'https://faceid-workshop.cognitiveservices.azure.com/face/v1.0/detect';
+  const params = `returnFaceId=true&returnFaceLandmarks=false&returnFaceAttributes=age,gender`;
+  const [url, setUrl] = React.useState('');
+  const[payload, setPayload] = React.useState<any[]>();
+
+  const ImageAPI= (): Promise<void> | void => {
+    fetch (`${endpoint}?${params}`, {
+        method: 'POST',
+        headers: {
+          'Ocp-Apim-Subscription-Key': apiKey,
+          'Content-Type': 'application/json',
+        },
+        body:JSON.stringify({url: url}),
+   }
+    ).then(async (response: Response) =>{
+        if(response.ok){
+            const result = await response.json();
+            setPayload(result);
+            console.log(result)
+       }
+        else{
+           console.log(url);
+       }
+    });
+  
+  };
+
+  const updateUrl = (_: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string | undefined): void => {
+    if (newValue !== undefined) {
+      setUrl(newValue);
+    }
+       };
+
+  return(
+    <div className='App'>
+        <title>Image-ine</title>
+        <div className='App-content'>
+            <h1>Welcome to Image-ine!</h1>
+            <p>The world's leading image analysis software developed by a guy named Jachi</p>
+          </div>
+          <div className='App-content'>
+          <form>
+          <TextField label="Please enter an image url" styles={{root:{marginTop: '10px'}}} onChange={updateUrl}/>
+          </form>
+          <PrimaryButton text="Identify" styles={{root:{float: 'right', marginTop: '10px'}}} onClick={ImageAPI}/>
+
+          <div className='ImageContent'>
+            <h2>Original Photo</h2>
+          <span><img alt='A person or group of people' src={url}/></span>
+          </div>
+
+        </div>
+        </div>
   );
 }
 
